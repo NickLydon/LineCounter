b8db9c47c2ebfbc92d20ee04f19054ebdf480f9b
NickLydon <nickkell@gmail.com>
2014-02-23 23:42:40 +0000

    Separated console app from library
    Separated functionality from UI

---
+++ Lib/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+module LineCounter
+
+open System.IO
+open Microsoft.FSharp.Collections
+
+let countLines directory searchPatterns = 
+    
+    searchPatterns
+        |> Seq.map (fun pattern -> Directory.EnumerateFiles(directory, pattern, SearchOption.AllDirectories))
+        |> Seq.flatten
+        |> Seq.map(fun file -> async {
+            use fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)
+            let data = Array.create(int fs.Length) 0uy
+            let! _ = fs.AsyncRead(data, 0, data.Length)
+            return data         
+        })
+        |> Async.Parallel 
+        |> Async.RunSynchronously
+        |> PSeq.map System.Text.Encoding.ASCII.GetString
+        |> Seq.map(fun fileContents -> fileContents.Split([| System.Environment.NewLine |], System.StringSplitOptions.RemoveEmptyEntries))
+        |> Seq.flatten
+        |> Seq.length
+    
--- Source/LineCounter/Program.fs
+++ Source/LineCounter/Program.fs
@@ -1,49 +1,28 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
-//open System
 open System.IO
-open Microsoft.FSharp.Control.CommonExtensions
-open Microsoft.FSharp.Collections
+open System.Diagnostics
+open LineCounter
 
 [<EntryPoint>]
 let main argv = 
-    //printfn "%A" argv
 
     if(argv.Length = 0) then failwith "Directory must be provided as first argument"
 
-    let timer = new System.Diagnostics.Stopwatch()
+    let timer = Stopwatch()
     timer.Start() 
     let directory = argv.[0]
-    let enumerateFiles pattern = Directory.EnumerateFiles(directory, pattern, SearchOption.AllDirectories)
 
     let searchPatterns = seq {
-        yield "*"
+        if (argv.Length = 1) then yield "*"
         yield! Seq.ofArray argv 
             |> Seq.skip 1 
             |> Seq.takeWhile (fun _ -> true)
     }
-   
-    let allFileContent = 
-        searchPatterns
-        |> Seq.map enumerateFiles
-        |> Seq.flatten
-        |> Seq.map(fun file -> async {
-            use fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)
-            let data = Array.create(int fs.Length) 0uy
-            let! _ = fs.AsyncRead(data, 0, data.Length)
-            return data         
-        })
-        |> Async.Parallel 
-        |> Async.RunSynchronously
-        |> PSeq.map System.Text.Encoding.ASCII.GetString
-                                
-    let lineCounts =
-        allFileContent
-        |> Seq.map(fun i -> i.Split([| System.Environment.NewLine |], System.StringSplitOptions.RemoveEmptyEntries))
-        |> Seq.flatten
-        |> Seq.length
-        
+                         
+    let lineCounts = countLines directory searchPatterns
+
     printfn "Lines: %i" lineCounts
 
     timer.Stop()
